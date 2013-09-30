7f78a1bc97482f2fa38974bd36716eac15218212
thomasolsen <thomas-amo@hotmail.com>
2013-09-30 09:20:55 +0200

    v2.1
    fikset js

--- javascript.js
+++ javascript.js
@@ -3,3 +3,9 @@ document.getElementById("text1").innerHTML = value;
 document.getElementById("text2").innerHTML = "Du har valgt";
 
 }
+
+function AlertIt() {
+var answer = confirm ("under forandring")
+if (answer)
+window.location="proglangs.html.html";
+}
